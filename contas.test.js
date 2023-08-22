2d3fcb209a08d60b58f27535eae2b1672cc65ad9
IagoSlps <iago.lopes8@fatec.sp.gov.br>
2023-08-21 21:49:32 -0300

    alteracoes no script

--- contas.test.js
+++ contas.test.js
@@ -1,5 +1,4 @@
-const { soma } = require('./contas');
-const contas = require('/comtas');
+const contas = require('./contas');
 
 test("soma 2 + 5 esperado 7 como reusultado",()=>{
     expect(contas.soma(2,5)).toBe(7)
